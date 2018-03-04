c67b209b974cb2e1d0c6f6a2a0940fcc0b53b001
Amimaro <amir.zahlan@gmail.com>
2018-03-04 14:36:47 -0300

    Check if loggedIn

--- client/app/pages/trades/trades.component.ts
+++ client/app/pages/trades/trades.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { AppService } from '../../services/app.service';
+
 @Component({
   selector: 'app-trades',
   templateUrl: './trades.component.html',
@@ -10,13 +12,22 @@ export class TradesComponent implements OnInit {
 
   title: string = "";
 
-  constructor(public route: ActivatedRoute) { }
+  constructor(public appService: AppService, public route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route
-      .data
-      .subscribe(data => {
-        this.title = data.trade;
+    this.appService.getSession().subscribe(
+      res => {
+        console.log(res);
+        // Get Data from Routing Module
+        this.route.data
+          .subscribe(data => {
+            this.title = data.trade;
+          });
+      },
+      err => {
+        console.log("Error occured");
+        console.log(err);
+        this.appService.routeTo(['/'])
       });
   }
 
